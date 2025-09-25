333c655135a2a66d88d44936ee7a758e4e3b6477
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2025-09-25 21:39:32 +0530

    Connected Firebase and installed required packages

--- Frontend/lumie/lib/main.dart
+++ Frontend/lumie/lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lumie/firebase_options.dart';
 import 'package:lumie/utils/app_colors.dart';
 import 'package:lumie/widgets/tab_screen.dart';
 
-void main() {
+void main() async {
+  // Firebase Initizing
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
